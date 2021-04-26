6f1bfa1b0e493f48ea6c0b51c6107a746271eaa8
Dmitry Kiselev <kiselev17.00@mail.ru>
2021-04-26 19:24:55 +0700

    Added Google Auth

--- app/src/main/java/ru/grande/PSLite/MainActivity.java
+++ app/src/main/java/ru/grande/PSLite/MainActivity.java
@@ -3,15 +3,36 @@ package ru.grande.PSLite;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TabHost;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.Task;
+
 public class MainActivity extends TabActivity{
 
+    private GoogleApiClient mGoogleApiClient;
+
+    private ImageView imgProfilePic;
+    private TextView txtName, txtEmail;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,6 +50,11 @@ public class MainActivity extends TabActivity{
                 .setContent(new Intent(this, TabSMS.class)));
 
         th.setCurrentTab(1);
+
+
     }
+
+
+
 }
 
--- app/src/main/java/ru/grande/PSLite/WifiPosition.java
+++ app/src/main/java/ru/grande/PSLite/WifiPosition.java
@@ -25,6 +25,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.Expose;
@@ -47,7 +54,6 @@ import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
-
 public class WifiPosition extends Activity implements View.OnClickListener {
 
     Context context;
@@ -61,6 +67,14 @@ public class WifiPosition extends Activity implements View.OnClickListener {
     String sAnswer = "", tosend;
     static String address = "http://195.149.207.161:8080/api/v1/";
     String recordTime;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int RC_SIGN_IN = 007;
+
+    GoogleSignInClient mGoogleSignInClient;
+
+    GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +84,11 @@ public class WifiPosition extends Activity implements View.OnClickListener {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         context = getApplicationContext();
         uemac = getMacAddr();
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        //signInButton.setSize(SignInButton.SIZE_STANDARD);
+
+        signInButton.setOnClickListener(this);
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         SimpleDateFormat timeFormat = new SimpleDateFormat("dd.MM.yy HH-mm-ss");
         wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
         Scan = findViewById(R.id.Scan);
@@ -378,6 +397,7 @@ public class WifiPosition extends Activity implements View.OnClickListener {
                         }
                     });
         }
+        if(id == R.id.sign_in_button)  signIn();
     }
 
     public static String getMacAddr() {
@@ -404,5 +424,50 @@ public class WifiPosition extends Activity implements View.OnClickListener {
         } catch (Exception ignored) {
         }
         return "02:00:00:00:00:00";
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Log.v("MSG:", "работает");
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Log.v("MSG:", "Или работает");
+        } catch (ApiException e) {
+            Log.v("MSG:", "Не работает");
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+        }
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personInfo = personName +" "+ personGivenName + " " +personFamilyName;
+            Log.v("MSG:", personInfo);
+        }
     }
 }
